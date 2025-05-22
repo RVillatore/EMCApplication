a5087eb43fe28d818e044b3beee968fdc904e484
RVillatore <97712059+RVillatore@users.noreply.github.com>
2025-05-22 16:13:30 +0200

    Update BuildPKG.sh

--- BuildPKG.sh
+++ BuildPKG.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-sudo apt update
-chown -R machinekit:machinekit /machinekit
+#sudo apt update
+#chown -R machinekit:machinekit /machinekit
+#cd /machinekit/emca
+#export PKG_CONFIG_PATH=/machinekit/emca
+#sudo update-alternatives --set fakeroot /usr/bin/fakeroot-tcp
+#./debian/configure machinekit-hal=0.5.21099-1.git2c2ff0e51~bullseye no-docs
+#mk-build-deps --remove --root-cmd sudo --tool 'apt-cudf-get --solver aspcud -o APT::Get::Assume-Yes=1 -o Debug::pkgProblemResolver=0 -o APT::Install-Recommends=0' debian/control
+#sudo apt install ./*.deb -y
+#dpkg-buildpackage -us -uc
+#cp ../*.deb .
+set -e
+
+# Setup environment
 cd /machinekit/emca
-export PKG_CONFIG_PATH=/machinekit/emca
+
+# Setup cloudsmith sources
+curl -1sLf 'https://dl.cloudsmith.io/public/machinekit/machinekit/cfg/setup/bash.deb.sh' | bash
+curl -1sLf 'https://dl.cloudsmith.io/public/machinekit/machinekit-hal/cfg/setup/bash.deb.sh' | bash
+
+# Build steps
+export PKG_CONFIG_PATH=/machinekit/emca/
 sudo update-alternatives --set fakeroot /usr/bin/fakeroot-tcp
 ./debian/configure machinekit-hal=0.5.21099-1.git2c2ff0e51~bullseye no-docs
 mk-build-deps --remove --root-cmd sudo --tool 'apt-cudf-get --solver aspcud -o APT::Get::Assume-Yes=1 -o Debug::pkgProblemResolver=0 -o APT::Install-Recommends=0' debian/control

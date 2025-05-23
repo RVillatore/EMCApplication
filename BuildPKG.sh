d8a82b8a9f597b532e14958b9195afc53b02e985
RVillatore <97712059+RVillatore@users.noreply.github.com>
2025-05-23 09:22:56 +0200

    Update BuildPKG.sh

--- BuildPKG.sh
+++ BuildPKG.sh
@@ -15,11 +15,17 @@ sudo update-alternatives --set fakeroot /usr/bin/fakeroot-tcp
 echo "Script executed from: ${PWD}"
 
 # Show files
-echo "File list current folder: `ls -lah .`}"
+echo "File list current folder: `ls -lah .`"
+
+# Show problem file permissions
+echo "Permissions for problem file before chmod are: `ls -lah ./debian/configure`"
 
 # Fix permissions before configure
 sudo chmod 755 debian
 sudo chmod 644 debian/*
+
+# Show problem file permissions
+echo "Permissions for problem file after chmod are: `ls -lah ./debian/configure`"
 
 # Configure with proper permissions
 ./debian/configure machinekit-hal=0.5.21099-1.git2c2ff0e51~bullseye no-docs

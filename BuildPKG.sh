f4356355042654b41f55b383ce5c261283a2dbae
RVillatore <97712059+RVillatore@users.noreply.github.com>
2025-05-23 11:33:44 +0200

    Update BuildPKG.sh

--- BuildPKG.sh
+++ BuildPKG.sh
@@ -27,6 +27,10 @@ sudo chmod 755 debian
 # Show problem file permissions
 #echo "Permissions for problem file after chmod are: `ls -lah ./debian/configure`"
 
+# Add MK repo
+curl -1sLf 'https://dl.cloudsmith.io/public/machinekit/machinekit-hal/setup.deb.sh' | sudo -E bash
+sudo apt update
+
 # Configure with proper permissions
 ./debian/configure machinekit-hal=0.5.21099-1.git2c2ff0e51~bullseye no-docs
 
@@ -37,17 +41,6 @@ mk-build-deps --remove --root-cmd sudo \
     -o Debug::pkgProblemResolver=0 \
     -o APT::Install-Recommends=0' \
     debian/control
-
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-# For Debian Stretch, Ubuntu 16.04 and later
-keyring_location=/usr/share/keyrings/machinekit-machinekit-hal-archive-keyring.gpg
-# For Debian Jessie, Ubuntu 15.10 and earlier
-keyring_location=/etc/apt/trusted.gpg.d/machinekit-machinekit-hal.gpg
-sudo curl -1sLf 'https://dl.cloudsmith.io/public/machinekit/machinekit-hal/gpg.D35981AB4276AC36.key' | sudo gpg --dearmor >> ${keyring_location}
-sudo curl -1sLf 'https://dl.cloudsmith.io/public/machinekit/machinekit-hal/config.deb.txt?distro=ubuntu&codename=xenial&component=main' > /etc/apt/sources.list.d/machinekit-machinekit-hal.list
-sudo chmod 644 ${keyring_location}
-sudo chmod 644 /etc/apt/sources.list.d/machinekit-machinekit-hal.list
-apt-get update
 
 sudo apt install machinekit-hal=0.5.21099-1.git2c2ff0e51~bullseye -y
     

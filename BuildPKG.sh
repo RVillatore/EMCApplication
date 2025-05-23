ae4c691932fb02550b9c4d8a5e2b1863bae42610
RVillatore <97712059+RVillatore@users.noreply.github.com>
2025-05-23 11:17:42 +0200

    Update BuildPKG.sh

--- BuildPKG.sh
+++ BuildPKG.sh
@@ -12,20 +12,20 @@ export PKG_CONFIG_PATH="$PWD"
 sudo update-alternatives --set fakeroot /usr/bin/fakeroot-tcp
 
 # show where we are
-echo "Script executed from: ${PWD}"
+#echo "Script executed from: ${PWD}"
 
 # Show files
-echo "File list current folder: `ls -lah .`"
+#echo "File list current folder: `ls -lah .`"
 
 # Show problem file permissions
-echo "Permissions for problem file before chmod are: `ls -lah ./debian/configure`"
+#echo "Permissions for problem file before chmod are: `ls -lah ./debian/configure`"
 
 # Fix permissions before configure
 sudo chmod 755 debian
 #sudo chmod 644 debian/*
 
 # Show problem file permissions
-echo "Permissions for problem file after chmod are: `ls -lah ./debian/configure`"
+#echo "Permissions for problem file after chmod are: `ls -lah ./debian/configure`"
 
 # Configure with proper permissions
 ./debian/configure machinekit-hal=0.5.21099-1.git2c2ff0e51~bullseye no-docs
@@ -45,7 +45,7 @@ sudo apt install -y apt-transport-https
 keyring_location=/usr/share/keyrings/machinekit-machinekit-hal-archive-keyring.gpg
 # For Debian Jessie, Ubuntu 15.10 and earlier
 keyring_location=/etc/apt/trusted.gpg.d/machinekit-machinekit-hal.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/machinekit/machinekit-hal/gpg.D35981AB4276AC36.key' |  gpg --dearmor >> ${keyring_location}
+curl -1sLf 'https://dl.cloudsmith.io/public/machinekit/machinekit-hal/gpg.D35981AB4276AC36.key' | sudo gpg --dearmor >> ${keyring_location}
 curl -1sLf 'https://dl.cloudsmith.io/public/machinekit/machinekit-hal/config.deb.txt?distro=ubuntu&codename=xenial&component=main' > /etc/apt/sources.list.d/machinekit-machinekit-hal.list
 sudo chmod 644 ${keyring_location}
 sudo chmod 644 /etc/apt/sources.list.d/machinekit-machinekit-hal.list

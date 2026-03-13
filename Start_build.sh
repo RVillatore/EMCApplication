df73bc0f8ba2ea705842ca1bf365740551ddb70b
RVillatore <97712059+RVillatore@users.noreply.github.com>
2026-03-13 13:07:58 +0100

    Update Start_build.sh

--- Start_build.sh
+++ Start_build.sh
@@ -22,8 +22,8 @@ export PKG_CONFIG_PATH=/machinekit/emca
 HAL_VER=$(apt-cache policy machinekit-hal | awk '/Candidate:/ {print $2}')
 ./debian/configure machinekit-hal=$HAL_VER no-docs
 #./debian/configure machinekit-hal=0.5.21099-1.git2c2ff0e51~bionic no-docs
-mk-build-deps -irs sudo -t 'apt-cudf-get --solver aspcud -o Debug::pkgProblemResolver=0 -o APT::Install-Recommends=0'
-#mk-build-deps -irs sudo -t 'apt -o Debug::pkgProblemResolver=0 -o APT::Install-Recommends=0'
+#mk-build-deps -irs sudo -t 'apt-cudf-get --solver aspcud -o Debug::pkgProblemResolver=0 -o APT::Install-Recommends=0'
+mk-build-deps -irs sudo -t 'apt -o Debug::pkgProblemResolver=0 -o APT::Install-Recommends=0'
 
 sudo update-alternatives --set fakeroot /usr/bin/fakeroot-tcp
 dpkg-buildpackage -us -uc
